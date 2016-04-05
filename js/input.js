6ff97ed1502fdb3ed14ce7ee1cb09119767845a0
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-04-05 10:41:34 -0700

    working on radio buttons and adding new HackerSpace obj

---
+++ js/input.js
@@ -0,0 +1,31 @@
+var addReviewButton = document.getElementById('add-review');
+var wifiRadioButtons, boozeRadioButtons, coffeeRadioButtons, tableSpaceRadioButtons, hoursRadioButtons, cashMoneyRadioButtons, googleReviewsRadioButtons;
+var buttonRows = [];
+buttonRows.push(wifiRadioButtons = document.getElementsByName('wi-fi'));
+buttonRows.push(boozeRadioButtons = document.getElementsByName('booze'));
+buttonRows.push(coffeeRadioButtons = document.getElementsByName('coffee'));
+buttonRows.push(tableSpaceRadioButtons = document.getElementsByName('table-space'));
+buttonRows.push(hoursRadioButtons = document.getElementsByName('hours'));
+buttonRows.push(cashMoneyRadioButtons = document.getElementsByName('cash-money'));
+buttonRows.push(googleReviewsRadioButtons = document.getElementsByName('google-review'));
+
+function handleReview(event){
+  event.preventDefault();
+  var inputName = event.target.businessName.value;
+  //console.log('input name = ' + inputName);
+  var inputAddress = event.target.address.value;
+  var inputWebsite = event.target.website.value;
+  var userReviewedLocation = new HackerSpace(inputName, inputAddress, inputWebsite);
+  var wifiValue = event.target.wifi.value;
+  var boozeValue = event.target.booze.value;
+  var coffeeValue = event.target.coffee.value;
+  var tableSpaceValue = event.target.tableSpace.value;
+  var hoursValue = event.target.hours.value;
+  var cashMoneyValue = event.target.cashMoney.value;
+  var googleReviewValue = event.target.googleReview.value;
+  console.log(userReviewedLocation);
+  userReviewedLocation.addRatings(boozeValue, coffeeValue, tableSpaceValue, googleReviewValue, cashMoneyValue, hoursValue, wifiValue);
+  console.log(userReviewedLocation);
+};
+
+addReviewButton.addEventListener('submit',handleReview);
