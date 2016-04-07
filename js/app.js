1d09dc8926362c31e6018197bb97fdc8da571f14
Chris Perez <chris.k.perez@gmail.com>
2016-04-07 11:04:21 -0700

    changed 0 reviews to .5 reviews

--- js/app.js
+++ js/app.js
@@ -49,26 +49,26 @@ HackerSpace.prototype.addRatings = function(booze, coffee, space, reviews, affor
 };
 
 new HackerSpace('Code Fellows', '2901 3rd Ave, Seattle, WA 98121', 'http://www.codefellows.org/').addRatings(3, 2.9, 5, 4.4, 4.5, 3.8, 3.2);
-new HackerSpace('Uptown Expresso', '2504 4th Ave, Seattle, WA 98121', 'http://www.velvetfoam.com/').addRatings(0, 4.7, 4.8, 4.3, 2.8, 4.0, 4.7);
-new HackerSpace('Street Bean Coffee', '2711 3rd Ave, Seattle, WA 98121', 'http://streetbeanespresso.org/').addRatings(0, 4.8, 2.5, 4.7, 4.1, 3.3, 4.0);
+new HackerSpace('Uptown Expresso', '2504 4th Ave, Seattle, WA 98121', 'http://www.velvetfoam.com/').addRatings(0.5, 4.7, 4.8, 4.3, 2.8, 4.0, 4.7);
+new HackerSpace('Street Bean Coffee', '2711 3rd Ave, Seattle, WA 98121', 'http://streetbeanespresso.org/').addRatings(0.5, 4.8, 2.5, 4.7, 4.1, 3.3, 4.0);
 new HackerSpace('Buckleys', '232 1st Ave W, Seattle, WA 98119',	'http://www.buckleyspubs.com/home-qa.html').addRatings(4.4, 1, 2.7, 4.2, 4.4, 3.9, 3.3);
 new HackerSpace('The 5 Point Cafe', '415 Cedar Street, Seattle, WA 98121', 'http://the5pointcafe.com/').addRatings(4.3, 1.2, 2.1, 3.9, 4.8, 5.0, 4.0);
-new HackerSpace('Holy Cannoli', '2720 3rd Ave, Seattle, WA 98121', 'http://www.holycannoliseattle.com/').addRatings(0, 5.0, 1.0, 4.5, 4.1, 2.8, 3.4);
+new HackerSpace('Holy Cannoli', '2720 3rd Ave, Seattle, WA 98121', 'http://www.holycannoliseattle.com/').addRatings(0.5, 5.0, 1.0, 4.5, 4.1, 2.8, 3.4);
 new HackerSpace('Tilikum Place Cafe', 'Tilikum Place Cafe', 'http://www.tilikumplacecafe.com/').addRatings(4.6, 4.4, 1.9, 4.3, 3.7, 3.5, 1.0);
-new HackerSpace('Cherry Street Coffee House', '2719 1st Ave, Seattle, WA 98121', 'http://cherryst.com/').addRatings(0, 4.3, 3.0, 4.0, 4.5, 3.6, 4.2);
+new HackerSpace('Cherry Street Coffee House', '2719 1st Ave, Seattle, WA 98121', 'http://cherryst.com/').addRatings(0.5, 4.3, 3.0, 4.0, 4.5, 3.6, 4.2);
 new HackerSpace('Drip City', '2929 1st Ave, Seattle, WA 98121', 'http://dripcitycoffee.com/').addRatings(3.5, 4.8, 4.5, 4.8, 4.4, 3.8, 4.8);
-new HackerSpace('Starbucks Denny Triangle', '521 Wall St, Seatt,e WA 98121', 'http://www.starbucks.com/store/114075/us/city-university/521-wall-st-seattle-wa-98121').addRatings(0, 4.2, 4, 4.2, 4.5, 3.9, 4.8);
+new HackerSpace('Starbucks Denny Triangle', '521 Wall St, Seatt,e WA 98121', 'http://www.starbucks.com/store/114075/us/city-university/521-wall-st-seattle-wa-98121').addRatings(0.5, 4.2, 4, 4.2, 4.5, 3.9, 4.8);
 
-new HackerSpace('Honor Coffee & Tea', '100 4th Ave N Seattle, WA 98109', 'http://www.honorcoffee.com/').addRatings(0, 4.4, 3.9, 4.4, 4.2, 3.8, 4.7);
-new HackerSpace('Artisan Coffee', '2523 5th Ave Seattle, WA 98121', 'http://www.artisancafeonvine.com/').addRatings(0, 4.3, 4.0, 4.4, 4.4, 3.5, 4.5);
-new HackerSpace('Bedlam Coffee', '2231 2nd Ave. Seattle, WA 98121', 'http://www.bedlamcoffee.com/').addRatings(0, 4.4, 4.5, 4.3, 4.3, 4.0, 4.4);
-new HackerSpace('Caffee Zingaro', '127 Mercer St. Seattle, WA 98109',	'https://www.yelp.com/biz/caffe-zingaro-seattle-3').addRatings(0, 4.5, 4.3, 4.4, 4.4, 3.7, 4.5);
+new HackerSpace('Honor Coffee & Tea', '100 4th Ave N Seattle, WA 98109', 'http://www.honorcoffee.com/').addRatings(0.5, 4.4, 3.9, 4.4, 4.2, 3.8, 4.7);
+new HackerSpace('Artisan Coffee', '2523 5th Ave Seattle, WA 98121', 'http://www.artisancafeonvine.com/').addRatings(0.5, 4.3, 4.0, 4.4, 4.4, 3.5, 4.5);
+new HackerSpace('Bedlam Coffee', '2231 2nd Ave. Seattle, WA 98121', 'http://www.bedlamcoffee.com/').addRatings(0.5, 4.4, 4.5, 4.3, 4.3, 4.0, 4.4);
+new HackerSpace('Caffee Zingaro', '127 Mercer St. Seattle, WA 98109',	'https://www.yelp.com/biz/caffe-zingaro-seattle-3').addRatings(0.5, 4.5, 4.3, 4.4, 4.4, 3.7, 4.5);
 new HackerSpace('Mamnoon Street', '2020 6th Ave, Seattle, WA 98121', 'http://www.mamnoonstreet.com').addRatings(4.5, 4.3, 4.3, 4.5, 3.9, 3.4, 4.2);
-new HackerSpace('Espresso Elegance', '122 Elliott Ave W. Seattle, WA 98119', 'http://www.espressoelegance.com').addRatings(0, 4.2, 4.4, 4.3, 4.4, 3.4, 4.3);
-new HackerSpace('Just Crepes', '2502 5th Ave. Seattle, WA 98121', 'https://www.yelp.com/biz/just-crepes-seattle').addRatings(0, 4.2, 3.9, 3.7, 4.5, 3.7, 4.2);
-new HackerSpace('The Seattle Grind', '516 Harrison St. Seattle, WA 98109', 'http://www.theseattlegrind.com/').addRatings(0, 4.3, 3.8, 4.0, 4.5, 3.6, 3.9);
+new HackerSpace('Espresso Elegance', '122 Elliott Ave W. Seattle, WA 98119', 'http://www.espressoelegance.com').addRatings(0.5, 4.2, 4.4, 4.3, 4.4, 3.4, 4.3);
+new HackerSpace('Just Crepes', '2502 5th Ave. Seattle, WA 98121', 'https://www.yelp.com/biz/just-crepes-seattle').addRatings(0.5, 4.2, 3.9, 3.7, 4.5, 3.7, 4.2);
+new HackerSpace('The Seattle Grind', '516 Harrison St. Seattle, WA 98109', 'http://www.theseattlegrind.com/').addRatings(0.5, 4.3, 3.8, 4.0, 4.5, 3.6, 3.9);
 new HackerSpace('Some Random Bar', '2604 1st Ave Seatt,e WA 98121', 'http://www.somerandombar.com').addRatings(4.6, 3.9, 3.6, 4.7, 4.5, 3.7, 3.8);
-new HackerSpace('Olympic Sculpture Park', '2901 Western Ave. Seattle, WA 98121', 'http://www.seattleartmuseum.org/visit/olympic-sculpture-park').addRatings(0, 0, 3.5, 4.8, 5, 3.8, 4.2);
+new HackerSpace('Olympic Sculpture Park', '2901 Western Ave. Seattle, WA 98121', 'http://www.seattleartmuseum.org/visit/olympic-sculpture-park').addRatings(0.5, 0.5, 3.5, 4.8, 5, 3.8, 4.2);
 
 console.log('hackerSpaceArray ', hackerSpaceArray);
 
